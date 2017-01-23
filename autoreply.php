b33119a1dbd40e86e3e11b53f155c904654cffac
Nopparoot Buasord <Nopparoot.B@samartcorp.com>
2017-01-24 00:30:24 +0700

    commit

--- autoreply.php
+++ autoreply.php
@@ -8,64 +8,63 @@ $events = json_decode($content, true);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
+        // Get replyToken
+		$replyToken = $event['replyToken'];
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text_in = $event['message']['text'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+		if ($event['type'] == 'message') {
+			
+            // Processing by message type.
+            switch ($event['message']['type']) {
+                case 'text':
+                    // Get message in.
+			        $text_in = $event['message']['text'];
+                    $text_out = getText($text_in);
+                    // Build message to reply back
+			        $messages = [
+				        'type' => 'text',
+				        'text' => $text_out
+			        ];                  
+                    break;
 
-            // Start check incoming message.
-            if (strlen($text_in)==1) {
-				
-				switch ($text_in) {
-					case '0':
-						$text_out = "0: (moon grin)";
-						break;
-					case '1':
-						$text_out = "1: (love)";
-						break;
-					case '2':
-						$text_out = "2: (hee)";
-						break;
-					case '3':
-						$text_out = "3: (sparkling eyes)";
-						break;
-					case '4':
-						$text_out = "4: (tongue out)";
-						break;
-					case '5':
-						$text_out = "5: (birthday)(birthday)";
-						break;
-					case '6':
-						$text_out = "6: (heart)(heart)(heart)(heart)";
-						break;
-					case '7':
-						$text_out = "7: (beer)(beer)(beer)";
-						break;
-					case '8':
-						$text_out = "8: (angry)";
-						break;
-					case '9':
-						$text_out = "9: (love) (rose stalk)(rose stalk)(rose stalk)";
-						break;
-					default:
-						$text_out = "(no)";
-				}
+                case 'image':
+                    $text_out = "ขอบคุณครับ สำหรับรูปภาพ";
+                    $messages = [
+				        'type' => 'text',
+				        'text' => $text_out
+			        ];                  
+                    break;
 
-			}
-			elseif (strtoupper($text_in) == "HELP") {
-				$text_out = "ต้องการให้ช่วยเรื่องอะไร \nเพียงแค่พิมพ์ข้อความดังต่อไปนี้เลยคร๊าบ (moon grin) \n\n1.เมนู_1 \n2.เมนู_2 \n3.เมนู_3 \n4.เมนู_4 \n5.เมนู_5 \n6.เมนู_6 \n7.เมนู_7 \n8.เมนู_8 \n9.เมนู_9 \n0.เมนู_0 \n\n(heart)\n และหากต้องการความช่วยเหลือด่วนติดต่อได้ 24 ชั่วโมง ที่นี่เลย \n(phone) 02-222-3333";
-			}
-			else {
-				$text_out = $text_in;
-			}
+                case 'video':
+                    $text_out = "ขอบคุณครับ สำหรับคลิปวีดีโอ";                
+                    $messages = [
+				        'type' => 'text',
+				        'text' => $text_out
+			        ];                  
+                    break;
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text_out
-			];
+                case 'audio':
+                    $text_out = "ขอบคุณครับ สำหรับคลิปเสียง";                
+                    $messages = [
+				        'type' => 'text',
+				        'text' => $text_out
+			        ];                  
+                    break;
+
+                case 'location':
+                    $text_out = "ขอบคุณครับ สำหรับการแจ้งพิกัดของคุณ";
+                    $messages = [
+				        'type' => 'text',
+				        'text' => $text_out
+			        ];                  
+                    break;
+
+                case 'sticker':
+                    // Build sticker to reply back
+                    $messages = getSticker();
+                    break;
+            }            
+
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -89,4 +88,60 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "Auto Reply Bot.";
+
+function getText($text_in) {
+    // Start check incoming message.
+    if (strlen($text_in)==1) {
+				
+		switch ($text_in) {
+			case '0':
+				$text_out = "0: (moon grin)";
+				break;
+			case '1':
+				$text_out = "1: (love)";
+				break;
+			case '2':
+				$text_out = "2: (hee)";
+				break;
+			case '3':
+				$text_out = "3: (sparkling eyes)";
+				break;
+			case '4':
+				$text_out = "4: (tongue out)";
+				break;
+			case '5':
+				$text_out = "5: (birthday)(birthday)";
+				break;
+			case '6':
+				$text_out = "6: (heart)(heart)(heart)(heart)";
+				break;
+			case '7':
+				$text_out = "7: (beer)(beer)(beer)";
+			    break;
+			case '8':
+				$text_out = "8: (angry)";
+				break;
+			case '9':
+				$text_out = "9: (love) (rose stalk)(rose stalk)(rose stalk)";
+				break;
+			default:
+				$text_out = "(no)";
+		}
+	}
+	elseif (strtoupper($text_in) == "HELP") {
+		$text_out = "ต้องการให้ช่วยเรื่องอะไร? \nเพียงแค่พิมพ์ข้อความดังต่อไปนี้เลยคร๊าบ (moon grin) \n\n1.เมนู_1 \n2.เมนู_2 \n3.เมนู_3 \n4.เมนู_4 \n5.เมนู_5 \n6.เมนู_6 \n7.เมนู_7 \n8.เมนู_8 \n9.เมนู_9 \n0.เมนู_0 \n\n(heart)\n และหากต้องการความช่วยเหลือด่วนติดต่อได้ 24 ชั่วโมง ที่นี่เลย \n(phone) 02-222-3333";
+	}
+	else {
+		$text_out = $text_in;
+	}
+
+    return $text_out;
+}
+
+function getSticker() {
+     $sticker = array(  'type' => 'sticker',
+                        'packageId' => '4',
+                        'stickerId' => '300' );
+     return $sticker;
+}
 
